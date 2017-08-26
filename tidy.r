2da406b6d77b2d0abe558c00e57bf8260d3c578c
Rensa <me@rensa.co>
2017-08-26 20:22:18 +1000

    Gapminder + Berkeley plot done (but HTML version is busted)

--- tidy.r
+++ tidy.r
@@ -1,4 +1,5 @@
-# build fancy steam train plot from downloaded gapminder and berkeley earth data
+#!/usr/bin/env Rscript --vanilla
+# download and combine gapminder and berkeley earth data
 # james goldie ('rensa'), august 2017
 
 library(RCurl)
@@ -7,9 +8,6 @@ library(tidyverse)
 library(magrittr)
 library(stringr)
 library(fuzzyjoin)
-library(gganimate)
-library(tweenr)
-library(viridis)
 filter = dplyr::filter # this is going to kill me one day
 source('util.r')
 
@@ -18,9 +16,6 @@ on_url = 'https://raw.githubusercontent.com/open-numbers/'
 berk_url = 'http://berkeleyearth.lbl.gov/auto/Regional/TAVG/Text/'
 year_start = 1900
 year_end = 2012
-frames_per_year = 1
-plot_font_1 = 'Helvetica Neue Light'
-plot_font_2 = 'Helvetica Neue'
 
 # create data directory if it's missing
 if(!dir.exists('data'))
@@ -178,77 +173,5 @@ all_data =
   inner_join(gapdata, temp,
     by = c('name', 'name_lowercase', 'year', 'match_dist'))
 write_csv(all_data, 'data/gapminder-berkeley-tidy.csv')
-
-message(run.time(), ' trimming excess data for bubble plot')
-bubble_data = all_data %>%
-  select(co2, pop_poorer_fraction, pop_fraction, year, emission_id) %>%
-  mutate(emission_year = year - year_start)
-bar_data = all_data %<>%
-  select(name, temp, pop_poorer_fraction, pop_fraction, year) %>%
-  mutate(
-    emission_year = year - year_start,
-    ease = 'exponential-in-out')
-
-message(run.time(), ' interpolating emissions and temp data for animation')
-bubble_tw = tween_appear(bubble_data, time = 'emission_year',
-  timerange = c(year_start, year_end),
-  nframes = frames_per_year * (year_end - year_start))
-bar_tw = tween_elements(bar_data, time = 'emission_year',
-  group = 'name', ease = 'ease',
-  nframes = frames_per_year * (year_end - year_start))
-
-message(run.time(), ' building emissions plot')
-stplot = ggplot() +
-  # temperature columns
-  geom_col(data = bar_tw,
-    aes(
-      x = pop_poorer_fraction + pop_fraction / 2,
-      fill = pop_poorer_fraction + pop_fraction / 2,
-      y = temp,
-      width = pop_fraction,
-      frame = .frame),
-    position = 'identity') +
-  # annual emission bubbles
-  geom_jitter(data = bubble_tw,
-    aes(
-      x = pop_poorer_fraction + pop_fraction / 2,
-      y = 1.75 * atan(2 * .age) + 0.3 * sin(2 * .age),
-      size = co2,
-      frame = .frame),
-    alpha = 0.1, width = 0, height = 0.1, colour = '#333333') +
-  geom_text(data = bar_tw,
-    aes(label = as.integer(.frame / frames_per_year + year_start),
-    frame = .frame, x = 0.5, y = 0.125),
-    size = 14, family = plot_font_2, fontface = 'bold') +
-  annotate('text', x = 0.0375, y = 0.125, size = 14, label = 'Poorest',
-    family = plot_font_1) +
-  annotate('text', x = 0.945, y = 0.125, size = 14, label = 'Wealthiest',
-    family = plot_font_1) +
-  scale_x_continuous(labels = scales::percent,
-    name = 'Percentile of GDP per capita (2010 $US)',
-      breaks = seq(0, 1, 0.25), limits = c(0, 1), expand = c(0, 0.02)) +
-  scale_y_continuous(breaks = 1:3, limits = c(0, 3.5),
-    name = 'Temperature anomaly (°C)', expand = c(0, 0)) +
-  scale_size_area(name = 'Annual CO2 emissions (excluding land use)',
-    max_size = 80, guide = FALSE) +
-  scale_fill_viridis(begin = 0.75, end = 1, limits = c(0, 1),
-    option = 'plasma', guide = FALSE) +
-  ggtitle('Historical CO2 emissions, GDP per capita and temperature rise',
-    subtitle = paste('Who contributed to the greenhouse effect,',
-      'and who suffers for it?')) +
-  theme_classic(base_size = 32, base_family = plot_font_1) +
-  theme(
-    plot.title = element_text(family = plot_font_2, face = 'bold'),
-    plot.margin = margin(rep(20, 4), 'px'),
-    axis.ticks.x = element_blank(),
-    axis.line.y = element_blank(),
-    axis.ticks.y = element_blank(),
-    panel.background = element_rect(fill = '#f8f9f9'),
-    plot.background = element_rect(fill = '#f8f9f9'))
-
-message(run.time(), ' rendering emissions plot')
-animation::ani.options(interval = 0.5 / frames_per_year)
-gganimate(stplot, '~/Desktop/steamtrain-out/bubble_gdppc_scaled.mp4',
-  ani.width = 1920, ani.height = 1080, title_frame = FALSE)
 
 message(run.time(), ' done!')
